b5c770648f4f8eda8eab158a8062181782e8d845
kumar <harshme78@gmail.com>
2022-07-28 01:03:05 +0530

    added upload to pinata

---
+++ client/src/Components/AddProduct.jsx
@@ -0,0 +1,205 @@
+// import Navbar from "./Navbar";
+import { useState,useEffect } from "react";
+import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
+import NFT_Digital_Warranty from '../NFT_Digital_Warranty.json';
+import { useLocation } from "react-router";
+
+export default function SellNFT () {
+    const [connected, toggleConnect] = useState(false);
+    const [currAddress, updateAddress] = useState('0x');
+    const [formParams, updateFormParams] = useState({ name: '', description: '', serialno: ''});
+    const [fileURL, setFileURL] = useState(null);
+    const ethers = require("ethers");
+    const [message, updateMessage] = useState('');
+    const location = useLocation();
+
+
+
+
+    async function connectWebsite() {
+
+        const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+        if(chainId !== '0x5')
+        {
+          //alert('Incorrect network! Switch your metamask network to Rinkeby');
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x5' }],
+         })
+        }  
+        await window.ethereum.request({ method: 'eth_requestAccounts' })
+          .then(() => {
+            // updateButton();
+            console.log("here");
+            getAddress();
+            window.location.replace(location.pathname)
+          });
+    }
+    
+    useEffect(() => {
+        let val = window.ethereum.isConnected();
+        if(val)
+        {
+          console.log("here");
+          getAddress();
+        //   toggleConnect(val);
+        //   updateButton();
+        }
+    
+        window.ethereum.on('accountsChanged', function(accounts){
+          window.location.replace(location.pathname)
+        })
+      });
+     
+    // useEffect( async () =>{
+
+    //     const ethers = require("ethers");
+        
+    
+    //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    //     const signer = provider.getSigner();
+    //     const addr = await signer.getAddress();
+
+    //     //Pull the deployed contract instance
+    //     let contract = new ethers.Contract(NFT_Digital_Warranty.address, NFT_Digital_Warranty.abi, signer)
+
+    //     //create an NFT Token
+    //     let transaction = await contract.getMyNFTs()
+
+    //     console.log(transaction);
+
+    // },[]);
+
+
+
+
+
+
+
+
+
+
+    //This function uploads the NFT image to IPFS
+    async function OnChangeFile(e) {
+        var file = e.target.files[0];
+        //check for file extension
+        try {
+            //upload the file to IPFS
+            const response = await uploadFileToIPFS(file);
+            if(response.success === true) {
+                console.log("Uploaded image to Pinata: ", response.pinataURL)
+                setFileURL(response.pinataURL);
+            }
+        }
+        catch(e) {
+            console.log("Error during file upload", e);
+        }
+    }
+
+    //This function uploads the metadata to IPDS
+    async function uploadMetadataToIPFS() {
+        const {name, description, serialno} = formParams;
+        //Make sure that none of the fields are empty
+        if( !name || !description || !serialno || !fileURL)
+            return;
+
+        const nftJSON = {
+            name, description, serialno, image: fileURL
+        }
+
+        try {
+            //upload the metadata JSON to IPFS
+            const response = await uploadJSONToIPFS(nftJSON);
+            
+            if(response.success === true){
+                console.log("Uploaded JSON to Pinata: ", response)
+                return response.pinataURL;
+            }
+        }
+        catch(e) {
+            console.log("error uploading JSON metadata:", e)
+        }
+    }
+
+    async function listNFT(e) {
+        e.preventDefault();
+
+        //Upload data to IPFS
+        try {
+            const metadataURL = await uploadMetadataToIPFS();
+            //After adding your Hardhat network to your metamask, this code will get providers and signers
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            updateMessage("Please wait.. uploading (upto 5 mins)")
+
+            //Pull the deployed contract instance
+            let contract = new ethers.Contract(NFT_Digital_Warranty.address, NFT_Digital_Warranty.abi, signer)
+
+            //massage the params to be sent to the create NFT request
+            // const serialno = ethers.utils.parseUnits(formParams.serialno, 'ether')
+            // let listingPrice = await contract.getListPrice()
+            // listingPrice = listingPrice.toString()
+
+            //actually create the NFT
+
+            const { serialno} = formParams;
+            let transaction = await contract.createToken(metadataURL, serialno, { value: '0' })
+           const r  = await transaction.wait()
+            console.log(transaction);
+            console.log(r);
+            alert("Successfully listed your NFT!");
+            updateMessage("");
+            updateFormParams({ name: '', description: '', serialno: ''});
+            // window.location.replace("/")
+        }
+        catch(e) {
+            alert( "Upload error"+e )
+        }
+    }
+
+
+    async function getAddress() {
+        const ethers = require("ethers");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const addr = await signer.getAddress();
+        updateAddress(addr);
+      }
+
+
+    console.log("Working", process.env);
+    return (
+        <div className="">
+        {/* <Navbar></Navbar> */}
+        <div className="" id="nftForm">
+            <form className="">
+            <h3 className="">Upload your NFT to the marketplace</h3>
+                <div className="">
+                    <label className="" htmlFor="name">NFT Name</label>
+                    <input className="" id="name" type="text" placeholder="Axie#4563" onChange={e => updateFormParams({...formParams, name: e.target.value})} value={formParams.name}></input>
+                </div>
+                <div className="mb-6">
+                    <label className="" htmlFor="description">NFT Description</label>
+                    <textarea className="" cols="40" rows="5" id="description" type="text" placeholder="Enter description" value={formParams.description} onChange={e => updateFormParams({...formParams, description: e.target.value})}></textarea>
+                </div>
+                <div className="mb-6">
+                    <label className="" htmlFor="SerialNo">Serial No </label>
+                    <input className="" type="text" placeholder="Enter Serial No"  value={formParams.serialno} onChange={e => updateFormParams({...formParams, serialno: e.target.value})}></input>
+                </div>
+                <div>
+                    <label className="" htmlFor="image">Upload Image</label>
+                    <input type={"file"} onChange={OnChangeFile}></input>
+                </div>
+                <br></br>
+                <div className="">{message}</div>
+                <button onClick={listNFT} className="">
+                    List NFT
+                </button>
+            </form>
+        </div>
+
+        <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={connectWebsite}>{connected? "Connected":"Connect Wallet"}</button>
+        <div>{currAddress}</div>
+        </div>
+    )
+}
---
+++ client/src/pinata.js
@@ -0,0 +1,93 @@
+//require('dotenv').config();
+// const key = process.env.REACT_APP_PINATA_KEY;
+// const secret = process.env.REACT_APP_PINATA_SECRET;
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: "e3bd9f4f1c70ec3e54d3",
+                pinata_secret_api_key: "087d698155867cbcb07fbc967330a9eee0f1938fd03495c5d701e9e9c3f2c886",
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    // https://api.pinata.cloud/pinning/pinFileToIPFS
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: "e3bd9f4f1c70ec3e54d3",
+                pinata_secret_api_key: "087d698155867cbcb07fbc967330a9eee0f1938fd03495c5d701e9e9c3f2c886",
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
